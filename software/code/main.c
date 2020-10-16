765878fbd5e91460ced0e0656f85350a66afaddb
ks6n19 <ks6n19@soton.ac.uk>
2020-10-16 21:27:18 +0100

    using fixed coordinates

--- software/code/main.c
+++ software/code/main.c
@@ -70,14 +70,14 @@ int main(void) {
   
   
 while(1) {
-  int x1 ;
-  int y1 ;
+  int x1 = 0 ;
+  int y1 = 0 ;
   
  
   int x2 = 50;
-  int y2 = 300;
+  int y2 = 30;
   
-  int x3 = 500;
+  int x3 = 50;
   int y3 = 70;
   
   
@@ -87,32 +87,23 @@ while(1) {
   
   int L1_positive, L2_positive, L3_positive ;
   
-   wait_for_any_switch_data();
-    
-    if ( check_switches(0) ) {
-      x1 =  read_switches(0) ;
-    }
-
-    if ( check_switches(1) ) {
-      y1  = read_switches(1);
-    }  
-
 for (int x = 0 ; x < 640 ; x++){
     for (int y = 0 ; y < 480 ; y++) {
     
 	  L1_detT   =   ((y2-y3) *  (x-x3)) + ((x3-x2) *  (y-y3)) ;
-    L2_detT   =   ((y3-y1) *  (x-x3)) + ((x1-x3) *  (y-y3)) ;
-    detT  =   	 ((y2-y3) * (x1-x3)) + ((x3-x2) * (y1-y3)) ;
+	  L2_detT   =   ((y3-y1) *  (x-x3)) + ((x1-x3) *  (y-y3)) ;
+   	  detT      =   ((y2-y3) * (x1-x3)) + ((x3-x2) * (y1-y3)) ;
 	   
-    L1_positive = ((L1_detT >= 0) == (detT >= 0)) ;
+   	  L1_positive = ((L1_detT >= 0) == (detT >= 0)) ;
 	  L2_positive = ((L2_detT >= 0) == (detT >= 0)) ;
 	  L3_positive = (((L1_detT + L2_detT) <= detT) == (detT >= 0)) ;
 	  
 	  
 	  if(L1_positive && L2_positive && L3_positive)
-        write_pix(x,y,1);
+        	write_pix(x,y,1);
 
-  } }
+  		} 
+	}
 
- }
+   }
 }        
