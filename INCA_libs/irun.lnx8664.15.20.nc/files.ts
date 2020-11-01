22cb7ecbfcf13930a2cc5235ba9a8344e1f813f1
ks6n19 <ks6n19@soton.ac.uk>
2020-11-01 10:22:41 +0000

    cleaned up unnecessary stuff

--- INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,2 +0,0 @@
-1601209925 /home/ks6n19/Documents/project/testbench/arm_soc_stim.sv
-1601287526 /home/ks6n19/Documents/project/testbench/de1_soc_wrapper_stim.sv
--- INCA_libs/irun.lnx8664.15.20.nc/ydir_files.ts
+++
@@ -1,1 +0,0 @@
-1602881525 behavioural
